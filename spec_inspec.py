7c053087ec06954f3e3a98fb31e723e6e3afd8fb
Aycha Tammour <aycha.tammour@gmail.com>
2016-02-26 13:49:48 -0500

    modified to read file names from a list in the working dir

--- spec_inspec.py
+++ spec_inspec.py
@@ -13,20 +13,25 @@ def spec_inspec(n1,n2):
     
     '''
 
-    t = Table.read("/Users/tammour/NLS1_sept2015/data-fig/NLS1-Data.fits") #read table with the names of the files containing the spectra and their fits
+    #t = Table.read("/Users/tammour/NLS1_sept2015/data-fig/NLS1-Data.fits") #read table with the names of the files containing the spectra and their fits
 
-    spec_ls= t['spSpec'] # list of the spectra files
+    #spec_ls= t['spSpec'] # list of the spectra files
+    #spec_ls= Table.read('plt_ls.txt', format= 'ascii')
+    spec_ls= open('plt_ls.txt', 'r')
+    s= spec_ls.readlines()
+    
     flags= [] # list of flags for bad fits
-    f_out = open("/Users/tammour/NLS1_sept2015/data-fig/obj_flg"+str(n1)+"_"+str(n2)+".txt", 'wr')
+    f_out = open("./obj_flg"+str(n1)+"_"+str(n2)+".txt", 'wr')
     
 
-    for s in spec_ls[n1:n2]:
+    for i in s[n1:n2]:
+        spec_name= i.rstrip()
         try:
 
-            spec= Table.read("/Users/tammour/HDstuff/atiaf-final-oct12-2011/data/"+s+".plt", format= 'ascii', data_start=2)
+            spec= Table.read(spec_name, format= 'ascii', data_start=2)
         
             fig= figure(figsize= (20,12))
-            ax1= plt.subplot2grid((10,10), (0, 0), colspan=10, rowspan=8)
+            ax1= subplot2grid((10,10), (0, 0), colspan=10, rowspan=8)
             ymax= max(spec['col2'])
             ax1.set_ylim(0, ymax)
             ax1.set_xlim(4500, 5400)
@@ -57,15 +62,15 @@ def spec_inspec(n1,n2):
             ylabel('Normalized flux')
         
         
-            ax2= plt.subplot2grid((10,10), (8, 0), colspan=10, rowspan=2)
+            ax2= subplot2grid((10,10), (8, 0), colspan=10, rowspan=2)
             ylabel('Residuals')
             ax2.set_ylim(-10,10)
             ax2.set_xlim(4500, 5400)
             scatter(spec['col1'], spec['col2']-spec['col4'], marker='+', color= '0.3')
-            print "plotting obj", s
+            print "plotting obj", spec_name
             flg= input("Good fit= 0, Bad fit= 1 \n flag= ")
             flags.append(flg)
-            f_out.write(s + ","+ str(flg) + '\n')
+            f_out.write(spec_name + ","+ str(flg) + '\n')
             
             resume = input("Press Enter to plot next spectrum on list.")
             clf()
